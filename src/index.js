8e09f60bedccaae7d1cb9163737f230ab248354d
Мурзагалиев Павел Серикович <murzagaliev@obalt.baltimore.ru>
2019-07-05 11:41:19 +0300

    Install bootsrap, reactstrap. Add component Nav

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
