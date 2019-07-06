d39b076454dc58c76bcd61804a50fb7c0491e755
Pavel S. Murzagaliev <pano696@gmail.com>
2019-07-07 01:25:56 +0300

    Add rudux provider store, reducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
+import {Provider} from 'react-redux';
+import store from './store';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ServiceDb, ServiceContext} from './services';
 import ErrorBoundry from './components/error-boundry';
@@ -11,13 +13,15 @@ import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
 const serviceDb = new ServiceDb();
 
 ReactDOM.render(
-  <ErrorBoundry>
-    <ServiceContext.Provider value={serviceDb}>
-      <Router>
-        <App />
-      </Router>
-    </ServiceContext.Provider>
-  </ErrorBoundry>
+  <Provider store={store}>
+    <ErrorBoundry>
+      <ServiceContext.Provider value={serviceDb}>
+        <Router>
+          <App />
+        </Router>
+      </ServiceContext.Provider>
+    </ErrorBoundry>
+  </Provider>
   , document.getElementById('root'));
 
 serviceWorker.unregister();
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -5,3 +5,5 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 
 }
+
+export default reducer;
