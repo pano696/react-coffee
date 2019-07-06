5de8f152397d435bfeffc91aa3be7a1e4b25f711
Pavel S. Murzagaliev <pano696@gmail.com>
2019-07-07 01:19:58 +0300

    Add error and spiner components, Error boundry

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import './index.css';
 import App from './components/app';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {ServiceDb, ServiceContext} from './services';
+import ErrorBoundry from './components/error-boundry';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
 
 const serviceDb = new ServiceDb();
 
 ReactDOM.render(
-  <ServiceContext.Provider value={serviceDb}>
-    <Router>
-      <App />
-    </Router>
-  </ServiceContext.Provider>
+  <ErrorBoundry>
+    <ServiceContext.Provider value={serviceDb}>
+      <Router>
+        <App />
+      </Router>
+    </ServiceContext.Provider>
+  </ErrorBoundry>
   , document.getElementById('root'));
 
 serviceWorker.unregister();
